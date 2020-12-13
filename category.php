9afb2d435d5ddb3ff14bb16f15447700471eee54
davy1ex <dezy1ex@gmail.com>
2020-12-14 00:25:11 +0400

    added blog page

--- category.php
+++ category.php
@@ -1,24 +1,30 @@
 <?php get_header() ?>
 
-<div id="page-title">
+<!-- <div id="page-title">
     <div class="row">
         <div class="ten columns centered text-center">
             <p>efdsfsdffssdfsdjf ksjgkldjfdlk;jgd;flgjrio gujeiorgjrlke gjk;ldfjg;df</p>
         </div>    
     </div>
-</div>
+</div> -->
 
+<div class="container">
+    <div class="posts">
+        <?php if (have_posts()) {
+            while (have_posts()) {
+                the_post(); ?>
+            <article class="post">
+                <h1><?php the_title() ?></h1>
 
-<div class="content-outer">
-    <div id="page-content" class="row">
-        <div id="primary" class="eight columns">
-            <?php if (have_posts()) {
-                while (have_posts()) {
-                    the_post();s
+                <?php the_content() ?>
+            </article>
+
+                <?
+                    }
                 }
-            }
-            
-            ?>
-        </div>
+                
+                ?>
     </div>
 </div>
+
+    
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-    
+    <?php if ( is_front_page() ) : ?>
 		<div class="wraper-swiper container">
             <div class="swiper-container">
                 <?php
@@ -20,8 +20,11 @@
         <div class="schedule container">
             <img src="<?php the_field('schedule') ?>" alt="">
         </div>
-	
-        
+	<?php else : ?>
+        <header class="page-header">
+            <h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
+        </header>
+	<?php endif; ?>
 
 
      
